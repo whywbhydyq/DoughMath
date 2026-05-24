fee3adbae75705b69a0af0f494ad06d5daeba52c
mir Y <2922027393@qq.com>
2026-05-25 02:23:44 +0800

    Fix remaining calculator wrapper types

--- src/components/calculators/PizzaDoughCalculator.tsx
+++ src/components/calculators/PizzaDoughCalculator.tsx
@@ -1,3 +1,3 @@
 'use client';
 import Calculator from '@/components/Calculator';
-export function PizzaDoughCalculator(){return <Calculator slug="pizza-dough-calculator"/>}
+export function PizzaDoughCalculator(){return <Calculator slug="pizza-dough-calculator" calculatorType="pizza-dough"/>}
