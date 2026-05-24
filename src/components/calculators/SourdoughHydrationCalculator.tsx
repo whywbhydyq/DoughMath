3e4f417e8395b2d501c11ad8df43f8802f5e77c8
mir Y <2922027393@qq.com>
2026-05-25 02:23:08 +0800

    Fix remaining calculator wrapper types

--- src/components/calculators/SourdoughHydrationCalculator.tsx
+++ src/components/calculators/SourdoughHydrationCalculator.tsx
@@ -1,3 +1,3 @@
 'use client';
 import Calculator from '@/components/Calculator';
-export function SourdoughHydrationCalculator(){return <Calculator slug="sourdough-hydration-calculator"/>}
+export function SourdoughHydrationCalculator(){return <Calculator slug="sourdough-hydration-calculator" calculatorType="sourdough-hydration"/>}
